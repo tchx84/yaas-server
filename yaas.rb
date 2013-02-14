6a4321d6eef6a3e69b680171f041ffd45bd04f44
Daniel Drake <dsd@laptop.org>
2013-02-14 15:35:45 -0600

    Remove daemons
    We'll rely on systemd for daemonization from this point.
    Simplify the code structure into fewer files.

--- yaas.rb
+++ yaas.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
-
 # Copyright Paraguay Educa 2010, Martin Abente
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,13 +13,100 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
+
+# Yet Another Activation System - Server
 
 require "logger"
-require File.join(File.dirname(__FILE__), "lib/yaas_server.rb")
+require "etc"
+require "yaml"
+require "openssl"
+require "webrick"
+require "webrick/https"
+require "xmlrpc/server"
+require File.join(File.dirname(__FILE__), "lib/yaas_agent.rb")
 
 log_path = File.join(File.dirname(__FILE__), "log/yaas.log")
 logger = Logger.new(log_path)
+
+class YaasServer
+
+    def initialize(config_path)
+        config                = YAML.load_file(config_path)
+
+        @bind_address         = config["bind_address"]
+        @port                 = config["port"]
+        @host_handler         = config["host_handler"]
+        @devkey_script_path   = config["devkey_script_path"]
+        @lease_script_path    = config["lease_script_path"]
+        @system_username      = config["system_username"]
+        @allowed_ip_addresses = config["allowed_ip_addresses"]
+        @private_key_path     = config["private_key_path"]
+        @certificate_path     = config["certificate_path"]
+        @secret_keyword       = config["secret_keyword"]
+        @num_threads          = config["num_threads"]
+    end
+
+    def run
+        switch_user()
+
+        private_key_content = File.open(@private_key_path).read
+        private_key         = OpenSSL::PKey::RSA.new(private_key_content)
+
+        certificate_content = File.open(@certificate_path).read
+        certificate         = OpenSSL::X509::Certificate.new(certificate_content)
+
+        handler             = YaasAgent.new(@devkey_script_path, @lease_script_path, @secret_keyword, @num_threads)
+        servlet             = XMLRPC::WEBrickServlet.new
+        server              = WEBrick::HTTPServer.new(
+
+          :Port             => @port,
+          :BindAddress      => @bind_address,
+          :SSLEnable        => true,
+          :SSLVerifyClient  => OpenSSL::SSL::VERIFY_NONE,
+          :SSLCertificate   => certificate,
+          :SSLPrivateKey    => private_key
+        )
+
+        setup_security(servlet)
+        servlet.add_handler(@host_handler, handler)
+        server.mount("/RPC2", servlet)
+
+        trap("INT"){ server.shutdown }
+        trap("TERM"){ server.shutdown }
+
+        server.start
+    end
+
+    private
+
+    def switch_user()
+        user = Etc::getpwnam(@system_username)
+
+        Process::Sys::setegid(user.gid)
+        Process::Sys::setgid(user.gid)
+        Process::Sys::setuid(user.gid)
+
+        if Process::Sys::getuid == 0
+            raise "You can NOT run this service as root"
+        end
+    end
+
+    def setup_security(servlet)
+
+        if !@allowed_ip_addresses.is_a?(Array)
+            raise "Allowed IP addresses must be an array"
+        end
+
+        if @allowed_ip_addresses.length == 0
+            @allowed_ip_addresses.push("127.0.0.1")
+        end
+
+        #Hack for bug related to ruby's xmlrpc/server.rb (supports regexp only)
+        regexp_ip_addresses = @allowed_ip_addresses.map { |address| Regexp.new(address) }
+        servlet.set_valid_ip(*regexp_ip_addresses)
+    end
+
+end
 
 begin
     config_file = File.join(File.dirname(__FILE__), "etc/yaas.config")
